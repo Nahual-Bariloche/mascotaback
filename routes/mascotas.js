3bcd9d846d4b3a784e65618293b5090d8ddf4bb4
Pedro Wood <mail_pedro@yahoo.com.ar>
2017-10-03 13:43:13 -0300

    dynamodb

--- routes/mascotas.js
+++ routes/mascotas.js
@@ -1,12 +1,33 @@
 var express = require('express');
+var AWS = require("aws-sdk");
 var router = express.Router();
+
+AWS.config.update({
+  region: "us-east-1",
+  endpoint: "https://dynamodb.us-east-1.amazonaws.com"
+});
+
+var docClient = new AWS.DynamoDB.DocumentClient();
+
+var table = "nahual-mobilehub-241192654-Locations";
 
 /* Obtener listado de mascotas */
 router.get('/', (req, res, next) => {
-  let pipo = { nombre: "pipo", raza: "perro", extraviado: "25/09/2017" };
-  let pupi = { nombre: "pupi", raza: "caniche toy", extraviado: "24/09/2017" };
-  res.json([pipo, pupi]);
+
+  var params = {
+      TableName: table
+  };
+  docClient.scan(params, function (err, data) {
+      if (err) {
+          console.error("Unable to scan the table. Error JSON:", JSON.stringify(err, null, 2));
+      } else {
+          console.log("Scan succeeded.");
+          res.json(data);
+      }
+  });
+
 });
+
 
 /* Agregar una mascota */
 router.post('/', (req, res) => {
