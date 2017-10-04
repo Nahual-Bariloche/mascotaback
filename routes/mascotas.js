5cfd16b3c9bc2ec78241bd247d1be125e7f4f769
Pedro Wood <mail_pedro@yahoo.com.ar>
2017-10-03 22:43:43 -0300

    identity pool

--- routes/mascotas.js
+++ routes/mascotas.js
@@ -2,11 +2,14 @@ var express = require('express');
 var AWS = require('aws-sdk');
 var router = express.Router();
 
+AWS.config.credentials = new AWS.CognitoIdentityCredentials({
+    IdentityPoolId: 'us-east-1:c520d5fc-4aa5-4ec1-bd6f-b29bca765720',
+    RoleArn: 'arn:aws:iam::533832295765:role/Cognito_DynamoPoolUnauth'
+});
+
 AWS.config.update({
     region: 'us-east-1',
-    endpoint: 'https://dynamodb.us-east-1.amazonaws.com',
-    accessKeyId: 'AKIAIKIN7JTZPDKNIHQA',
-    secretAccessKey: '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'
+    endpoint: 'https://dynamodb.us-east-1.amazonaws.com'
 });
 
 var docClient = new AWS.DynamoDB.DocumentClient();
@@ -19,20 +22,21 @@ router.get('/', (req, res, next) => {
     var params = {
         TableName: table
     };
-    res.json([
-        { nombre: 'pipo', raza: 'perro', extraviado: '11/11/2016' },
-        { nombre: 'pupi', raza: 'chihuahua', extraviado: '11/09/2017' },
-        { nombre: 'blanca', raza: 'labrador', extraviado: '01/10/2017' }
-    ]);
+    // res.json([
+    //     { nombre: 'pipo', raza: 'perro', extraviado: '11/11/2016' },
+    //     { nombre: 'pupi', raza: 'chihuahua', extraviado: '11/09/2017' },
+    //     { nombre: 'blanca', raza: 'labrador', extraviado: '01/10/2017' }
+    // ]);
 
-    // docClient.scan(params, function (err, data) {
-    //     if (err) {
-    //         console.error('Unable to scan the table. Error JSON:', JSON.stringify(err, null, 2));
-    //     } else {
-    //         console.log('Scan succeeded.');
-    //         res.json(data);
-    //     }
-    // });
+    docClient.scan(params, function (err, data) {
+        if (err) {
+            console.error('Unable to scan the table. Error JSON:', JSON.stringify(err, null, 2));
+            res.json(err);
+        } else {
+            console.log('Scan succeeded.');
+            res.json(data);
+        }
+    });
 
 });
 
